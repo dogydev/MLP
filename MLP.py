df333c948d91a99814e5a6d66ec45ae4a68012b3
dogydev <41112844+dogydev@users.noreply.github.com>
2018-08-18 09:34:35 +0100

    Added Project.

--- MLP.py
+++ MLP.py
@@ -1,1 +1,38 @@
-No code yet
+from numpy import random, dot, exp
+
+
+class MLP(object):
+
+    def __init__(self):
+        self.weights = 2 * random.random((3, 1)) - 1
+
+        self.lr = 0.003
+
+        print("Random Weights: ")
+        print(self.weights)
+
+    def train_weights(self, target, inputs):
+        for i in range(len(target) * 100000):
+            result = self.feedforward(inputs)
+            error = target - result
+
+            print()
+            print("Error: ")
+            print(error)
+            print()
+
+            weights_adjustment = dot(inputs.T, error * self.lr * self.sigmoid_deriv(result))
+
+            self.weights += weights_adjustment
+
+            print("New Weights: ")
+            print(self.weights)
+
+    def feedforward(self, inputs):
+        return self.sigmoid_function(dot(inputs, self.weights))
+
+    def sigmoid_function(self, x):
+        return 1 / (1 + exp(-x))
+
+    def sigmoid_deriv(self, x):
+        return x * (1 - x)
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-No Code Yet
+from random import randint
+from MLP import MLP
+from numpy import array
+
+mlp = MLP()
+
+rInput = randint(0, 1)
+inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
+training_data = array([[0, 1, 1, 0]]).T
+
+mlp.train_weights(training_data, inputs)
+
+new_input = array((input('X:'), input('Y:'), input('Z:')), dtype=int)
+new_input_result = mlp.feedforward(new_input)
+
+print()
+print("New input values: ")
+print(new_input)
+print()
+print("New input result: ")
+print(new_input_result)
